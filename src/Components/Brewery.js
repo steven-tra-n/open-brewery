7062b419b16f8e03a08b8292aa37e499dc1ad12e
Steven Tran <steventran9102@gmail.com>
2020-02-18 23:11:31 -0500

    Added test marker and infowindows

--- src/Components/Brewery.js
+++ src/Components/Brewery.js
@@ -1,79 +1,125 @@
 import React from 'react';
 import GoogleMapReact from 'google-map-react';
 
-const Map = ({ text }) => (
-    <div style={{
-      color: 'white', 
-      background: 'grey',
-      padding: '15px 10px',
-      display: 'inline-flex',
-      textAlign: 'center',
-      alignItems: 'center',
-      justifyContent: 'center',
-      borderRadius: '100%',
-      transform: 'translate(-50%, -50%)'
-    }}>
-      {text}
-    </div>
-  );
+// function Map({ text }){
+//   return(
+//     <div 
+//     // style={{
+//     //   color: 'white', 
+//     //   background: 'grey',
+//     //   padding: '15px 10px',
+//     //   display: 'inline-flex',
+//     //   textAlign: 'center',
+//     //   alignItems: 'center',
+//     //   justifyContent: 'center',
+//     //   borderRadius: '100%',
+//     //   transform: 'translate(-50%, -50%)'
+//     // }}
+//     >
+//       {text}
+//     </div>
+//   );
+// };
 
 class Brewery extends React.Component {
-    componentDidMount() {
-      this.getBrewery();
+  constructor(){
+    super();
+    this.state = { //Why use state here?
+      center: {lat: 40.2732, lng: -76.8867}, //Default center for map
+      zoom: 12
+    };
+  };
 
-      this.setState({
-        center: {lat: 40.2732, lng: -76.8867}, //Default center for map
-        zoom: 12
+  componentDidMount() {
+    this.getBrewery();
+  };
+
+  getBrewery() {
+    this.setState({
+      id: this.props.match.params.id
+    }, () => {
+      fetch(`https://api.openbrewerydb.org/breweries/${this.props.match.params.id}`)
+        .then(res => res.json())
+        .then((result) => {
+          this.setState({ 
+            brewery: result,
+            center: {
+              lat: parseFloat(result.latitude),
+              lng: parseFloat(result.longitude)
+            },
+            zoom: 12
+          });
+        }, (error) => {
+          this.setState({ error });
+        }
+        );
+    });
+  };
+  
+  handleGoogleApiLoaded = ({map, maps}) => {
+    this.map = map;
+    this.maps = maps;
+    this.infoWindow = new maps.InfoWindow();
+
+    // let service = new maps.places.PlacesService(map);
+
+    // service.nearbySearch({
+    //   location: this.state.center,
+    //   radius: 2000,
+    //   types: ['school']
+    // }, this.callback);
+
+    // let callback = (results, status) => {
+    //   for(let i = 0; i < results.length; i++){
+    //     this.renderMarkers(results[i]);
+    //   };
+    // };
+
+    let renderMarkers = () => {
+      let marker = new this.maps.Marker({
+        position: this.state.center,
+        map: this.map,
+        title: 'Hello World!'
+      });
+
+      marker.addListener('click', () =>{
+        this.infoWindow.setContent('test');
+        this.infoWindow.open(this.map, marker);
       });
     };
-  
-    getBrewery() {
-      this.setState({
-        id: this.props.match.params.id
-      }, () => {
-        fetch(`https://api.openbrewerydb.org/breweries/${this.props.match.params.id}`)
-          .then(res => res.json())
-          .then((result) => {
-            this.setState({ 
-              brewery: result,
-              center: {
-                lat: parseFloat(result.latitude),
-                lng: parseFloat(result.longitude)
-              },
-              zoom: 12
-            });
-          }, (error) => {
-            this.setState({ error });
-          }
-          );
-      });
-    };
-  
-    render() {
-      if (this.state == null || this.state.brewery == null) return null;
-      const {name, street, city, state, phone, latitude, longitude} = this.state.brewery;
 
-      return (
-        <div>
-          <p>{this.state.id}</p>
-          <p>{name}</p>
-          <p>{street}</p>
-          <p>{city}</p>
-          <p>{state}</p>
-          <p>{phone}</p>
-          <div style={{ height: '100vh', width: '100%' }}>
-            <GoogleMapReact 
-                bootstrapURLKeys={{ key: process.env.REACT_APP_MAPS_API_KEY }}
-                defaultCenter={this.state.center} defaultZoom={this.state.zoom}>
-                <Map 
-                    lat={latitude} 
-                    lng={longitude}  
-                    text={name}
-                    center={latitude, longitude}
-                />
-            </GoogleMapReact>
-          </div>
-        </div>)
+  renderMarkers();
+
+  };
+
+  render() {
+    if (this.state == null || this.state.brewery == null) return null;
+    const {name, street, city, state, phone, latitude, longitude} = this.state.brewery;
+
+    return (
+      <div>
+        <p>{this.state.id}</p>
+        <p>{name}</p>
+        <p>{street}</p>
+        <p>{city}</p>
+        <p>{state}</p>
+        <p>{phone}</p>
+        <div style={{ height: '100vh', width: '100%' }}>
+          <GoogleMapReact 
+              bootstrapURLKeys={{ 
+                key: process.env.REACT_APP_MAPS_API_KEY
+              }}
+              defaultCenter={this.state.center} defaultZoom={this.state.zoom}
+              onGoogleApiLoaded={this.handleGoogleApiLoaded}>
+              {/* <Map 
+                  lat={latitude} 
+                  lng={longitude}  
+                  text={name}
+                  center={latitude, longitude}
+              /> */}
+          </GoogleMapReact>
+        </div>
+      </div>)
     };
   };
 
