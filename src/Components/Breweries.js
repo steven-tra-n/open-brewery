4e507fef8e043bf46d909938ce810ef6d91e721a
Tran <steventran9102@gmail.com>
2020-02-21 16:16:06 -0500

    Added search functionality

--- src/Components/Breweries.js
+++ src/Components/Breweries.js
@@ -2,79 +2,76 @@ import React from 'react';
 import RenderBreweries from './RenderBreweries';
 
 class Breweries extends React.Component {
-    componentDidMount() {
-      this.listBreweries();
-    };
-  
-    listBreweries() {
-      fetch("https://api.openbrewerydb.org/breweries?per_page=50?by_state=pennsylvania")
-        .then(res => res.json())
-        .then(
-          (result) => {
-            this.setState({
-              breweries: result
-            });
-          },
-          (error) => {
-            this.setState({
-              isLoaded: true,
-              error
-            });
-          }
-        );
-    };
+  constructor() {
+    super();
+    this.handleChangeFromBreweries = this.handleChangeFromBreweries.bind(this); //How can we remove this binding via arrow function?
+  };
 
-    SearchBreweries() {
-      // this.setState({
-      //   value: event.target.value
-      // });
+  componentDidMount() {
+    this.listBreweries();
+  };
 
-      // if(event.key === 'Enter') {
-      //   fetch(`https://api.openbrewerydb.org/breweries/search?query=${this.state.value}`)
-      //   .then(res => res.json())
-      //   .then((result) => {
-      //     // this.setState({ 
-      //     //   breweries: result
-      //     // });
-      //     this.props.breweries = result;
-      //   }, (error) => {
-      //     this.setState({ error });
-      //   });
-      // };
-
-      return (
-        <input placeholder='Search Breweries' onKeyDown={(event) => this.handleChange(event)} />
+  listBreweries() {
+    fetch("https://api.openbrewerydb.org/breweries?per_page=50?by_state=pennsylvania")
+      .then(res => res.json())
+      .then((result) => {
+          this.setState({
+            breweries: result
+          });
+        }, (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
       );
-    };
-  
-    render() {
-      if (this.state == null || this.state.breweries == null) return null;
-  
-      return (
-        <div>
-          <h1 id='title'>Breweries</h1>
-          <this.SearchBreweries breweries={this.state.breweries} />
-          <table id='breweries'>
-            <tbody>
-              <RenderBreweries breweries={this.state.breweries} />
-            </tbody>
-          </table>
-        </div>
-      );
+  };
+
+  handleChangeFromBreweries(e) {
+    if(e.key === 'Enter') {
+      fetch(`https://api.openbrewerydb.org/breweries/search?query=${e.target.value}`)
+      .then(res => res.json())
+      .then((result) => {
+        this.setState({ 
+          breweries: result
+        });
+      }, (error) => {
+        this.setState({ error });
+      });
     };
   };
 
-  // class SearchBreweries extends React.Component {
-  //   constructor(props){
-  //     super(props);
-  //     this.state = {
-  //       value: ''
-  //     };
-  //   };
-    
-  //   render() {
-      
-  //   };
-  // };
+  render() {
+    if (this.state == null || this.state.breweries == null) return null;
+
+    return (
+      <div>
+        <h1 id='title'>Breweries</h1>
+        <SearchBreweries handleChangeFromBreweries={this.handleChangeFromBreweries} />
+        <table id='breweries'>
+          <tbody>
+            <RenderBreweries breweries={this.state.breweries} />
+          </tbody>
+        </table>
+      </div>
+    );
+  };
+};
+
+class SearchBreweries extends React.Component {
+  constructor(props){
+    super(props);
+  };
+
+  handleChangeFromSearchBreweries(e) {
+    this.props.handleChangeFromBreweries(e);
+  };
+  
+  render() {
+    return (
+      <input placeholder='Search Breweries' onKeyDown={(e) => this.handleChangeFromSearchBreweries(e)} />
+    );
+  };
+};
 
   export default Breweries;
