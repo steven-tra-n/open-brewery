30bc2e2a8c2511faf6c281ac3f70c7249834e4b1
Tran <steventran9102@gmail.com>
2020-02-25 17:13:55 -0500

    Added BreweryList file

---
+++ src/Components/BreweryList.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+
+  function BreweryList(props) {
+    return props.breweries.map((brewery) => {
+      const {id, name, brewery_type, street, city, state, website_url} = brewery;
+
+      return (
+        <tr key={id}>
+          <Link to={`/breweries/${id}`}><td>{name}</td></Link>
+          <td>{brewery_type}</td>
+          <td>{street}</td>
+          <td>{city}</td>
+          <td>{state}</td>
+          <td><a href={website_url}>{website_url}</a></td>
+        </tr>
+      );
+    });
+  };
+
+  export default BreweryList;
