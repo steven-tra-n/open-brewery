96fa0a35e999357345364730b444394bde534caf
Tran <steventran9102@gmail.com>
2020-02-25 17:14:10 -0500

    Added SearchBar file

---
+++ src/Components/SearchBar.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import AutoComplete from './AutoComplete';
+
+class SearchBar extends React.Component {
+    constructor(props){
+      super(props);
+
+      this.state = {
+          suggestions: []
+      };
+    };
+  
+    handleOnKeyUp(e) {
+      this.props.onChange(e);
+
+      fetch(`https://api.openbrewerydb.org/breweries/autocomplete?query=${e.target.value}`)
+      .then(res => res.json())
+      .then((result) => {
+          this.setState({
+            suggestions: result
+          });
+        }, (error) => {
+          this.setState({ error });
+        }
+      );
+    };
+    
+    render() {
+      return (
+        <div>
+            <input placeholder='Search Breweries' onKeyUp={(e) => this.handleOnKeyUp(e)} />
+            <AutoComplete brewerySuggestions={this.state.suggestions} onSearch={this.props.onSearch}  />
+        </div>
+      );
+    };
+  };
+
+  export default SearchBar;
